769071923ce8adf2f4ac28b9861932a39628a780
XiaGF <felixxg@foxmail.com>
2017-04-23 14:04:00 +0800

    home

---
+++ app/src/main/java/com/teamwork/doubanapp_4a/home/util/GlideImageLoader.java
@@ -0,0 +1,18 @@
+package com.teamwork.doubanapp_4a.home.util;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.youth.banner.loader.ImageLoader;
+
+/**
+ * Created by XiaGF on 2017/4/22.
+ */
+
+public class GlideImageLoader extends ImageLoader {
+    @Override
+    public void displayImage(Context context, Object path, ImageView imageView) {
+        Glide.with(context).load(path).into(imageView);
+    }
+}
